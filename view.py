24c64b9345a4b74ff7d2c7a8052572fc2993184a
Vincent <vincentzhang515@gmail.com>
2022-07-19 23:25:42 -0700

    b1 change done

--- view.py
+++ view.py
@@ -24,3 +24,25 @@ def view(grid):
                 		raise AssertionError
             
         print()
+	for i in range(len(grid)):
+        	for j in range(len(grid[i])):
+            
+            		if grid[i][j] == EMPTY:
+                print("  ", end = "")
+                    
+            elif grid[i][j] == WALL:
+                print("##", end = "")
+                    
+            elif grid[i][j] == START:
+                print("^^", end = "")
+                    
+            elif grid[i][j] == END:
+                print("$$", end = "")
+                    
+            elif grid[i][j] == VISITED:
+                print("..", end = "")
+                    
+            else:
+                raise AssertionError
+            
+        print()
